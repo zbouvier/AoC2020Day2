63dac26845698d6f7f473a5d2b1a189da8896ed5
Zach Bouvier <zbouvier99@gmail.com>
2020-12-02 09:49:52 -0600

    cleanup

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def partOne():
     f = open('values.txt', 'r+')
     count = 0
-    for line in f.readlines():
+    for line in f.readlines(): #some REGEX stuff can cleanup like 85% of this code may do later
         minValueOfCharacters = line.split("-")[0]
         maxValueOfCharacters = line.split("-")[1].split(" ")[0]
         targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
@@ -17,18 +17,18 @@ def partOne():
 
 def partTwo():
     f = open('values.txt', 'r+')
-    validCount = 0
-    for line in f.readlines():
+    count = 0
+    for line in f.readlines(): #some REGEX stuff can cleanup like 85% of this code may do later
         minValueOfCharacters = int(line.split("-")[0]) - 1
         maxValueOfCharacters = int(line.split("-")[1].split(" ")[0]) - 1
         targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
         targetLine = line.split("-")[1].split(" ")[2].rstrip()
         if(targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters] == targetCharacter):
-            pass
+            pass # skips the double whammies
         elif((targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters != targetCharacter]) or (targetLine[maxValueOfCharacters] == targetCharacter and targetLine[minValueOfCharacters != targetCharacter])):
             validCount+=1
     f.close()
-    return validCount
+    return count
 
 
 
