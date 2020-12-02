453379e1d3c81598064abdccdf27bd3827981ef7
Zach Bouvier <zbouvier99@gmail.com>
2020-12-02 09:46:53 -0600

    cleanup

--- calc.py
+++ calc.py
@@ -1,48 +1,37 @@
-import re
-f = open('values.txt', 'r+')
 def partOne():
+    f = open('values.txt', 'r+')
     count = 0
     for line in f.readlines():
-        woFirstValue = line.split("-")
-        minValueOfCharacters = woFirstValue[0]
-        woSecondValue = woFirstValue[1].split(" ")
-        maxValueOfCharacters = woSecondValue[0]
-        targetCharacter = woSecondValue[1].split(":")[0]
-        #print(woSecondValue)
-        targetLine = woSecondValue[2].rstrip()
-        #print(woSecondValue[2])
-        #print(minValueOfCharacters + "  " + maxValueOfCharacters + "  " + targetCharacter + "  " + targetLine)
+        minValueOfCharacters = line.split("-")[0]
+        maxValueOfCharacters = line.split("-")[1].split(" ")[0]
+        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
+        targetLine = line.split("-")[1].split(" ")[2].rstrip()
         charCount = 0
         for ch in targetLine:
-            #print("comp " + ch + " to "+ targetCharacter)
             if(ch == targetCharacter):
                 charCount=charCount+1
-        #print(charCount)
         if((charCount <= int(maxValueOfCharacters) and charCount >= int(minValueOfCharacters)) or (charCount <= int(minValueOfCharacters) and charCount >= int(maxValueOfCharacters))):
             count+=1
+    f.close()
     return count
 
 def partTwo():
+    f = open('values.txt', 'r+')
     validCount = 0
     for line in f.readlines():
-        woFirstValue = line.split("-")
-        minValueOfCharacters = int(woFirstValue[0]) - 1
-        woSecondValue = woFirstValue[1].split(" ")
-        maxValueOfCharacters = int(woSecondValue[0]) - 1
-        targetCharacter = woSecondValue[1].split(":")[0]
-        targetLine = woSecondValue[2].rstrip()
+        minValueOfCharacters = int(line.split("-")[0]) - 1
+        maxValueOfCharacters = int(line.split("-")[1].split(" ")[0]) - 1
+        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
+        targetLine = line.split("-")[1].split(" ")[2].rstrip()
         if(targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters] == targetCharacter):
-            print(str(minValueOfCharacters) + "  " + str(maxValueOfCharacters) + "  " + targetCharacter + "  " + targetLine + " is invalid")
-        elif(targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters != targetCharacter]):
+            pass
+        elif((targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters != targetCharacter]) or (targetLine[maxValueOfCharacters] == targetCharacter and targetLine[minValueOfCharacters != targetCharacter])):
             validCount+=1
-        elif(targetLine[maxValueOfCharacters] == targetCharacter and targetLine[minValueOfCharacters != targetCharacter]):
-            validCount+=1
-        else:
-            print(str(minValueOfCharacters) + "  " + str(maxValueOfCharacters) + "  " + targetCharacter + "  " + targetLine + " is invalid")
+    f.close()
     return validCount
 
 
 
+print(partOne())
 print(partTwo())
 
-f.close()
