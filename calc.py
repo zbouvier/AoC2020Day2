f04dd2dcc7489d53c344a104677f9c8b73f4add5
Zach Bouvier <zbouvier99@gmail.com>
2020-12-02 09:59:51 -0600

    day 2

--- calc.py
+++ calc.py
@@ -2,10 +2,10 @@ def partOne():
     f = open('values.txt', 'r+')
     count = 0
     for line in f.readlines(): #some REGEX stuff can cleanup like 85% of this code may do later
-        minValueOfCharacters = line.split("-")[0]
-        maxValueOfCharacters = line.split("-")[1].split(" ")[0]
-        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
-        targetLine = line.split("-")[1].split(" ")[2].rstrip()
+        minValueOfCharacters = line.split("-")[0] #12
+        maxValueOfCharacters = line.split("-")[1].split(" ")[0] #15
+        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0] #q
+        targetLine = line.split("-")[1].split(" ")[2].rstrip() #qqqqqqqqqqxq
         charCount = 0
         for ch in targetLine:
             if(ch == targetCharacter):
@@ -19,14 +19,12 @@ def partTwo():
     f = open('values.txt', 'r+')
     count = 0
     for line in f.readlines(): #some REGEX stuff can cleanup like 85% of this code may do later
-        minValueOfCharacters = int(line.split("-")[0]) - 1
-        maxValueOfCharacters = int(line.split("-")[1].split(" ")[0]) - 1
-        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0]
-        targetLine = line.split("-")[1].split(" ")[2].rstrip()
-        if(targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters] == targetCharacter):
-            pass # skips the double whammies
-        elif((targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters != targetCharacter]) or (targetLine[maxValueOfCharacters] == targetCharacter and targetLine[minValueOfCharacters != targetCharacter])):
-            validCount+=1
+        minValueOfCharacters = int(line.split("-")[0]) - 1 #12 - 1
+        maxValueOfCharacters = int(line.split("-")[1].split(" ")[0]) - 1 #15 - 1
+        targetCharacter = line.split("-")[1].split(" ")[1].split(":")[0] #q
+        targetLine = line.split("-")[1].split(" ")[2].rstrip() #qqqqqqqqqqxq
+        if (targetLine[minValueOfCharacters] == targetCharacter and targetLine[maxValueOfCharacters] != targetCharacter) ^ (targetLine[maxValueOfCharacters] == targetCharacter and targetLine[minValueOfCharacters] != targetCharacter):
+            count+=1 #Uses XOR operator ^ 
     f.close()
     return count
 
